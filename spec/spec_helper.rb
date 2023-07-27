63efe8efd358839e85fe5202add2c551cf8537d3
Derek Kniffin <derek@quimbee.com>
2023-07-27 13:12:56 -0400

    Fix specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require "bundler/setup"
-require "importmap/version/manager"
+require "importmap-package-manager"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
